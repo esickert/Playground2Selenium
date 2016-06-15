56362ff4b59606906e794fd45602971a5799f928
esickert <esickert@gmail.com>
2016-06-14 17:28:13 -0700

    more

--- SeleniumTest/src/seleniumTestPackage/SeleniumTest.java
+++ SeleniumTest/src/seleniumTestPackage/SeleniumTest.java
@@ -1,23 +1,23 @@
-package seleniumTestPackage; //name of the package in which class files are stored
+package seleniumTestPackage; 		  //name of the package in which class files are stored
 
 import org.openqa.selenium.firefox.*; // import necessary Webdriver classes
 import org.openqa.selenium.WebDriver; // import necessary Webdriver classes
 
-public class SeleniumTest {  //class declaration
+public class SeleniumTest {  				  //class declaration
 
 	public static void main(String[] args) {  //start of the main method
 		// TODO Auto-generated method stub
 		
-		WebDriver driver = new FirefoxDriver(); // concept implementation is done 
+		WebDriver driver = new FirefoxDriver(); // implementation is done 
 												// in 2 classes: RemoteWebDriver and 
-												// HtmlUnitDriver
+												// HtmlUnitDriver. Firefox driver is a 
+												// subclass of RemoteWebDriverClass.
 		
 		MoreSeleniumTestClass stuff = new MoreSeleniumTestClass(); 
 		
 		driver.get("http://www.bing.com");
 		stuff.Sleep(5);
 		driver.quit();
-		
 
 	}
 
