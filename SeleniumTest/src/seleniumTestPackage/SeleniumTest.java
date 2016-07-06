15a346c765f7fa16c26435eea76e8e66d9b67f3a
esickert <esickert@gmail.com>
2016-07-05 17:37:10 -0700

    more

--- SeleniumTest/src/seleniumTestPackage/SeleniumTest.java
+++ SeleniumTest/src/seleniumTestPackage/SeleniumTest.java
@@ -1,12 +1,15 @@
-package seleniumTestPackage; 		  //name of the package in which class files are stored
+package seleniumTestPackage; 		         //name of the package in which class files are stored
 
-import org.openqa.selenium.firefox.*; // import necessary Webdriver classes
-import org.openqa.selenium.WebDriver; // import necessary Webdriver classes
+import org.openqa.selenium.firefox.*;        // import necessary Webdriver classes
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;        // import necessary Webdriver classes
+import org.openqa.selenium.WebElement;       // need to import WebElement class to use it...see below.
 
-public class SeleniumTest {  				  //class declaration
+public class SeleniumTest {  				 //class declaration
 
-	public static void main(String[] args) {  //start of the main method
+	public static void main(String[] args) { //start of the main method
 		// TODO Auto-generated method stub
+		
 		
 		WebDriver driver = new FirefoxDriver(); // implementation is done 
 												// in 2 classes: RemoteWebDriver and 
@@ -14,11 +17,20 @@ public class SeleniumTest {  				  //class declaration
 												// subclass of RemoteWebDriverClass.
 		MoreSeleniumTestClass stuff = new MoreSeleniumTestClass(); 
 		
-		driver.get("http://www.bing.com");
+		driver.get("http://www.yahoo.com");
 		stuff.Sleep(5);
 		stuff.setWindowSize(driver);
 		stuff.Sleep(5);
 		stuff.resizeWindow(driver);
+		stuff.Sleep(10);
+		
+//		WebElement searchBox = driver.findElement(By.name("q")); //WebElement class needs to be imported ... see above
+//		WebElement searchBox = driver.findElement(By.id("uh-search-box")); //finding WebElement by id, yahoo search box using firebug
+		WebElement searchBox = driver.findElement(By.name("p")); //finding WebElement by name, yahoo search box (using firebug)
+		searchBox.sendKeys("Microsoft");
+		stuff.Sleep(10);
+		searchBox.submit();
+		stuff.Sleep(10);
 		
 		
 		driver.quit();
