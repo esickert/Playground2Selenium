2b85e3fd04644345a5967e017275d1b6304a2c91
esickert <esickert@gmail.com>
2016-07-14 16:03:10 -0700

    By.clasName()

--- SeleniumTest/src/seleniumTestPackage/SeleniumTest.java
+++ SeleniumTest/src/seleniumTestPackage/SeleniumTest.java
@@ -24,36 +24,42 @@ public class SeleniumTest {  				 //class declaration
 //		driver.get("http://www.duckduckgo.com");
 //		driver.navigate().to("http://www.yahoo.com");// <Webdriver Instance>.navigate().to("www. ... .com")
 		samantha.get("http://www.google.com");
-		stuff.Sleep(5);
+//		stuff.Sleep(5);
 //		stuff.setWindowSize(driver);
-		stuff.Sleep(5);
+//		stuff.Sleep(5);
 //		stuff.resizeWindow(driver);
-		stuff.Sleep(5);
+//		stuff.Sleep(5);
 //****************************************************************************************
 //		see page 20 - 24 in book		
 //		WebElement searchBox = driver.findElement(By.name("q")); //WebElement class needs to be imported ... see above. (google searchbox)
-		WebElement searchBox = samantha.findElement(By.id("lst-ib")); //By.id method to find Google search box Used chrome
+//		**********************************************************************************	
+//		WebElement searchBox = samantha.findElement(By.id("lst-ib")); //By.id method to find Google search box Used chrome
 //		WebElement searchBox = driver.findElement(By.id("uh-search-box")); //finding WebElement by id, yahoo search box using firebug
 //		WebElement searchBox = driver.findElement(By.name("p")); //finding WebElement by name, yahoo search box (using firebug using yahoo)
 //		WebElement searchBox = driver.findElement(By.className("js-search-input")); //finding WebElement by className(using firefox on DuckDuckGo) 
 //		WebElement searchBox = driver.findElements(By.className("js-search-input")); //don't know what's wrong here!!!!!!
-		
+//		**********************************************************************************
+//	Locating the same element by different mechanisms. (same web element- google search box) 
+//		WebElement searchBox = samantha.findElement(By.name("q"));
+		WebElement searchBox = samantha.findElement(By.className("gsfi"));
 //****************************************************************************************		
-		List<WebElement> input = samantha.findElements(By.tagName("input"));
+/**		List<WebElement> input = samantha.findElements(By.tagName("input"));
 		System.out.println("The inputs List size is: " + input.size());
 		for (int i = 0; i <= input.size()-1; i++) {
 			System.out.println(input.get(i));
-		}
-				
+		} **/
+		stuff.Sleep(5);
 		searchBox.sendKeys("Bing");
 		//******************************************************************************************************
 		stuff.Sleep(5);
-//		searchBox = driver.findElement(By.name("btnk"));  //trying to locate "Google Search" button on Google page
+//		searchBox = samantha.findElement(By.className("l"));  //trying to locate "Google Search" button on Google page
 //		samantha.findElement(By.className("r")).click();                                      //this DOESN'T WORK!!
-
+//		stuff.Sleep(5);
 
 		searchBox.submit();
 //		stuff.Sleep(5);
+		samantha.get("www.bing.com");   /this is a hack!!!!!
+		stuff.Sleep(5);
 		
 		
 		samantha.quit();
